a41babee462a85e1a73eaf1aa6545bc25feded95
mickaelyoshua <mickaelyoshua@gmail.com>
2026-01-13 09:50:44 -0300

    environment variables load

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
+use dotenvy::dotenv;
+
+mod db;
+mod models;
+
 fn main() {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+    println!("{database_url}");
     println!("Hello, world!");
 }
