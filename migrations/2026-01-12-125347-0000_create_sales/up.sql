10c164410fc4cd1926873ed0013affc17e9dc37e
mickaelyoshua <mickaelyoshua@gmail.com>
2026-01-15 08:50:40 -0300

    include new fields to sales table

--- migrations/2026-01-12-125347-0000_create_sales/up.sql
+++ migrations/2026-01-12-125347-0000_create_sales/up.sql
@@ -6,8 +6,14 @@ CREATE TABLE sales (
     total DECIMAL(10,2) NOT NULL CHECK (total >= 0),
     payment_method payment_method NOT NULL,
     notes TEXT,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX idx_sales_seller_id ON sales(seller_id);
 CREATE INDEX idx_sales_created_at ON sales(created_at DESC);
+
+CREATE TRIGGER update_sales_updated_at
+	BEFORE UPDATE ON sales
+	FOR EACH ROW
+	EXECUTE FUNCTION update_updated_at_column();
