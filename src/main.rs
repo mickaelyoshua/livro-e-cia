38dc34b2a6cbb10e44b31bc71c9ee9acb95847fe
mickaelyoshua <mickaelyoshua@gmail.com>
2026-01-23 09:01:33 -0300

    setup of server

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,41 @@
-use std::env;
-
 use dotenvy::dotenv;
+use rocket::launch;
+use rocket_dyn_templates::Template;
+use tracing::info;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
+use crate::{auth::JwtConfig, config::AppConfig, db::init_pool};
+
+mod auth;
+mod config;
 mod db;
 mod error;
 mod models;
+mod repositories;
 mod routes;
 mod schema;
 
-fn main() {
+#[launch]
+fn rocket() -> _ {
     dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
-    println!("{database_url}");
-    println!("Hello, world!");
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let conf = AppConfig::from_env();
+    info!(is_production = %conf.is_production, "App configuration loaded");
+
+    let pool = init_pool(&conf.database_url).expect("Failed to create database pool");
+
+    let jwt_conf = JwtConfig::from_env().expect("Failed to load JWT configuration");
+
+    info!("Start Livro e Cia server");
+
+    rocket::build()
+        .manage(pool)
+        .manage(jwt_conf)
+        .manage(conf)
+        .attach(Template::fairing())
 }
